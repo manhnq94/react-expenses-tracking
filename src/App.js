835c36f376ea2079cb2516fd1c43b1b021eea09c
manhnq94 <manhnq94@gmail.com>
2021-08-12 14:48:48 +0700

    split ExpenseItem into smaller components

--- src/App.js
+++ src/App.js
@@ -26,17 +26,17 @@ function App() {
                 date={expenses[0].date}
                 title={expenses[0].title}
                 amount={expenses[0].amount}
-            ></ExpenseItem>
+            />
             <ExpenseItem
                 date={expenses[1].date}
                 title={expenses[1].title}
                 amount={expenses[1].amount}
-            ></ExpenseItem>
+            />
             <ExpenseItem
                 date={expenses[2].date}
                 title={expenses[2].title}
                 amount={expenses[2].amount}
-            ></ExpenseItem>
+            />
         </div>
     );
 }
