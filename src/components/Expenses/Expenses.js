40036e7d80698f4fca2f955633b2d6a698156e27
manhnq94 <manhnq94@gmail.com>
2021-08-13 14:27:08 +0700

    add conditional

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,22 +15,28 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filterYear;
     });
 
+    let expensesContent = <p>No expenses found!</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => {
+            return (
+                <ExpenseItem
+                    key={expense.id}
+                    date={expense.date}
+                    title={expense.title}
+                    amount={expense.amount}
+                />
+            );
+        });
+    }
+
     return (
         <Card className="expenses">
             <ExpensesFilter
                 onFilterYearChange={handleFilterYearChange}
                 currentSelectedYear={filterYear}
             />
-            {filteredExpenses.map((expense) => {
-                return (
-                    <ExpenseItem
-                        key={expense.id}
-                        date={expense.date}
-                        title={expense.title}
-                        amount={expense.amount}
-                    />
-                );
-            })}
+            {expensesContent}
         </Card>
     );
 }
